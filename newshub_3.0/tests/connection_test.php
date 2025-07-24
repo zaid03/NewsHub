25da7a8dd75092ed2c283c81342fa6f6dcda0b53
zaid03 <terguyzaid@gmail.com>
2025-07-24 21:05:54 +0100

    Remove unused database connection and news fetching tests

--- newshub_3.0/tests/connection_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'C:/2.me_codes/1.Project _Fin_Etude/contento v2.0/src/php/connect.php';
-
-if ($conn) {
-    echo "Connection successful!";
-} else {
-    echo "Connection failed!";
-}
-?>
